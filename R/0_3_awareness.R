1204daa4dfb6c1ae5a44752498366107c4b03cf0
Edgar <edgar.steiger+github@gmail.com>
2021-01-29 14:42:18 +0100

    update awareness data (weekwise now)

--- R/0_3_awareness.R
+++ R/0_3_awareness.R
@@ -1,31 +1,91 @@
 library(tidyverse)
+library(lubridate)
 
 mydatapath <- "./data/awareness/"
 
-trends_alle_bl <- read_delim(paste0(mydatapath, "trends_alle_bl.csv"), 
-                             ";", 
-                             escape_double = FALSE, 
-                             col_types = cols(BL_ID = col_integer(), 
-                                              Coronatrend = col_double(), 
-                                              Datum = col_date(format = "%Y-%m-%d")), 
-                             locale = locale(encoding = "UTF-8"),
-                             trim_ws = TRUE)
-relativ_gesamt_2020_03_13 <- read_csv(paste0(mydatapath, 
-                                             "relativ_gesamt_2020-03-13.csv"))
+bundeslaender_maps <- tibble(
+  BL_ID=1:16,
+  Bundesland=c(
+    "Schleswig-Holstein",
+    "Hamburg",
+    "Niedersachsen",
+    "Bremen",
+    "Nordrhein-Westfalen",
+    "Hessen",
+    "Rheinland-Pfalz",
+    "Baden-Württemberg",
+    "Bayern",
+    "Saarland",
+    "Berlin",
+    "Brandenburg",
+    "Mecklenburg-Vorpommern",
+    "Sachsen",
+    "Sachsen-Anhalt",
+    "Thüringen"
+  ),
+  Kuerzel=c(
+    "SH",
+    "HH",
+    "NI",
+    "HB",
+    "NW",
+    "HE",
+    "RP",
+    "BW",
+    "BY",
+    "SL",
+    "BE",
+    "BB",
+    "MV",
+    "SN",
+    "ST",
+    "TH"
+  )
+)
 
-relativ_bl_2020_03_13 <- left_join(trends_alle_bl %>% 
-                                     filter(Datum=="2020-03-13"), 
-                                   relativ_gesamt_2020_03_13,
+trends_alle_bl <- tibble()
+
+for (kuerzel in bundeslaender_maps$Kuerzel) {
+  this_bl <- read_csv(paste0("data/awareness/trends_", kuerzel, ".csv"), 
+                      col_names = c("woche", "searches"),
+                      col_types = cols(woche=col_date(format = "%Y-%m-%d"), 
+                                       searches=col_double()), 
+                      skip = 2) %>%
+    mutate(searches=ifelse(is.na(searches), 0, searches),
+           BL_ID=bundeslaender_maps%>%filter(Kuerzel==kuerzel)%>%pull(BL_ID),
+           Kuerzel=kuerzel,
+           Bundesland=bundeslaender_maps%>%filter(Kuerzel==kuerzel)%>%pull(Bundesland)) %>%
+    drop_na()
+  trends_alle_bl <- bind_rows(trends_alle_bl,
+                              this_bl)
+}
+
+relativ_gesamt_2020_03_15 <- read_csv(paste0(mydatapath, 
+                                             "relativ_gesamt_2020-03-15.csv"))
+
+relativ_bl_2020_03_15 <- left_join(trends_alle_bl %>% 
+                                     filter(woche=="2020-03-15"), 
+                                   relativ_gesamt_2020_03_15,
                                    by="Bundesland") %>%
-  mutate(baseline20200313=Coronatrend*Relativtrend/100) %>%
-  rename(trend20200313=Coronatrend) %>%
-  dplyr::select(BL_ID, baseline20200313, trend20200313)
+  mutate(baseline20200315=searches*Relativtrend/100) %>%
+  rename(trend20200315=searches) %>%
+  dplyr::select(BL_ID, baseline20200315, trend20200315)
 
 relativ_bl <- left_join(trends_alle_bl, 
-                        relativ_bl_2020_03_13, 
+                        relativ_bl_2020_03_15, 
                         by="BL_ID") %>%
-  mutate(relativtrend=baseline20200313/trend20200313*Coronatrend) %>%
-  rename(date=Datum) %>%
-  dplyr::select(date, BL_ID, Bundesland, relativtrend)
+  mutate(relativtrend=baseline20200315/trend20200315*searches) %>%
+  rename(date=woche) %>%
+  dplyr::select(date, BL_ID, Bundesland, relativtrend) %>%
+  mutate(year=year(date),
+         kw=isoweek(date))
+
+all_days <- tibble(date=as_date(as_date("2020-01-01"):max(relativ_bl$date))) %>%
+  mutate(year=year(date),
+         kw=isoweek(date))
+
+searches_data <- all_days %>%
+  left_join(relativ_bl, by=c("year", "kw")) %>%
+  dplyr::select(date=date.x, BL_ID, Bundesland, relativtrend)
 
 write_csv(relativ_bl, paste0(mydatapath, "awareness.csv"))
